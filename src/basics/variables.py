076a75bf1d032f9663c9ab709d6e1b54faff90ed
Georgi Ivanov <georgi.ivanov85@hotmail.com>
2023-07-04 16:50:48 +0200

    variables basics

---
+++ src/basics/variables.py
@@ -0,0 +1,12 @@
+greeting = "Hello"
+name = "Georgi"
+age = 24
+print(age)
+
+print(type(greeting))
+print(type(age))
+
+age_in_words = "2 years"
+
+print(name + " is " + age + " years old")
+print(type(age))
